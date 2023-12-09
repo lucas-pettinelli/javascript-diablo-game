0404221827394fe2d1cc5c66dfc4609420042cad
lucas-pettinelli <pettinellilucas@gmail.com>
2023-12-08 21:50:48 -0300

    first commit

---
+++ js/login.js
@@ -0,0 +1,34 @@
+const input = document.querySelector('.login_input');
+const button = document.querySelector('.login_button');
+const form = document.querySelector('.login-form');
+const button_effect = document.querySelectorAll("button");
+
+const audio = new Audio("/media/diablo_2_diablo_boss.mp3");
+
+button_effect.forEach(b => {
+    b.addEventListener("click", () => {
+        audio.play();
+    });
+});
+
+
+const validateInput = ({ target }) => {
+    if (target.value.length > 2) {
+        button.removeAttribute('disabled');
+    } else {
+        button.setAttribute('disabled', '')
+    }
+}
+
+const sendHandle = (event) => {
+    event.preventDefault();
+
+    localStorage.setItem('player', input.value);
+    setTimeout(() => {
+        window.location = 'pages/game.html';
+    }, 5500);
+
+}
+
+input.addEventListener('input', validateInput);
+form.addEventListener('submit', sendHandle);
